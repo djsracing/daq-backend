92a6bdfb50594c07406cb421ff64b5fdf455de87
blackchapel <kc.dark@protonmail.com>
2022-09-18 09:37:06 +0530

    jwt middleware

--- src/routers/admin.router.js
+++ src/routers/admin.router.js
@@ -1,9 +1,10 @@
 const express = require('express');
+const auth = require('./../middlewares/authentication.middleware');
 const { generateInvitationCode } = require('./../controllers/admin.controller');
 
 // Initializing router
 const router = express.Router();
 
-router.post('/generate/invitation-code', generateInvitationCode);
+router.post('/generate/invitation-code', [auth.verifyJwt, auth.accountActivatedTrue], generateInvitationCode);
 
 module.exports = router;
